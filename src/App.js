3c23a59458fa2f1336f1ecb50a50eddee4283bc5
randallcoding <alexrandall2002@gmail.com>
2023-04-24 16:47:10 +0100

    tailwind landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="p-10 max-w-sm mx-auto bg-white rounded-xl text-center shadow-lg my-80">
+        <div className="shrink-0">
+        </div>
+        <div>
+          <div className="text-xl font-large text-black">Woodchuck Office</div>
+        </div>
+          <div className="p-10 max-w-sm mx-auto bg-white rounded-xl text-center shadow-lg my-2">
+              <div className="shrink-0">
+              </div>
+              <div>
+                  <div className="text-xl font-large text-black">Sign in</div>
+              </div>
+          </div>
+      </div>
   );
 }
 
