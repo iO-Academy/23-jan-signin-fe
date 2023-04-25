ca350a4e10264e85c81ad8ec84e56e0f0f22db8e
alreuben <ayannahreuben@gmail.com>
2023-04-25 17:41:57 +0100

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".final-project/23-jan-signin-be/src.{js,css}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#D9D9D9',
+                'secondary': '#DFC5DC',
+            },
+        },
+    },
+    plugins: [],
 }
 
