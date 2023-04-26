8fd18ae219c1323d51ee314319dd7aa6a53f5970
alreuben <ayannahreuben@gmail.com>
2023-04-26 09:09:56 +0100

    edited text box

--- src/SignInPage/index.js
+++ src/SignInPage/index.js
@@ -10,7 +10,7 @@ const SignInPage = () => {
             <section className="px-40 py-20">
                 <form action="/">
                     <label className="text-5xl">Name *</label><br></br>
-                    <input type="text" id="name" name="name" value="Enter your name here" className="flex justify-center h-14 w-full"/>
+                    <input type="text" id="name" name="name" className="flex justify-center h-14 w-full"/>
                     <label className="text-5xl">Company</label><br></br>
                     <input type="text" id="company" name="company" className="bg-white border-solid h-14 w-full"/><br></br>
                     <Link to="/SignedIn"><Button buttonText="Sign In"/></Link>
