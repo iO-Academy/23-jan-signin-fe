cba0e583060970e518160e4911756edb0c2a2091
alreuben <ayannahreuben@gmail.com>
2023-04-26 14:25:41 +0100

    front end to back end link complete and disable button funtionality

--- src/SignInPage/index.js
+++ src/SignInPage/index.js
@@ -10,12 +10,10 @@ const SignInPage = () => {
     const [message, setMessage] = useState('')
 
     const handleNameChange = (e) => {
-        console.log(e.target.value)
-        setCompany(e.target.value)
+        setName(e.target.value)
     }
 
     const handleCompanyChange = (e) => {
-        console.log(e.target.value)
         setCompany(e.target.value)
     }
 
@@ -24,11 +22,13 @@ const SignInPage = () => {
         try {
             let res = await fetch("http://127.0.0.1:4004/signin", {
                 method: "POST",
+                headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
                     name: name,
                     company: company,
                 }),
             })
+            console.log(res)
             let resJson = await res.json();
             if (res.status === 200) {
                 setName(name)
@@ -45,18 +45,15 @@ const SignInPage = () => {
     return (
         <>
             <Nav />
-            <section className="px-40 py-20">
+            <section className="px-40 py-20 bg-gray-100 mx-10">
                 <form action="/" method="POST" onSubmit={handleSubmit}>
                     <label className="text-5xl">Name *</label><br></br>
-                    <input onChange={handleNameChange} type="text" className="flex justify-center h-14 w-full border-solid border-2 border-black"/>
+                    <input onChange={handleNameChange} type="text" className="flex justify-center mt-5 mb-10 p-5 h-14 w-full border-solid border-2 border-black"/>
                     <label className="text-5xl">Company</label><br></br>
-                    <input onChange={handleCompanyChange} type="text" className="flex justify-center border-solid h-14 w-full border-solid border-2 border-black"/>
-
-                    {/*<Link to="/SignedIn">*/}
-                    <button type="submit" className="bg-black text-white px-10 py-5 text-xl tracking-widest hover:bg-white hover:text-black rounded">Sign In</button>
-                    {/*</Link>*/}
+                    <input onChange={handleCompanyChange} type="text" className="flex justify-center mt-5 p-5 border-solid h-14 w-full border-solid border-2 border-black"/>
+                    <Link to="/SignedIn"><button type="submit" className="bg-black text-white px-10 mt-10 py-5 text-xl tracking-widest hover:bg-white hover:text-black rounded" disabled={name !== '' ? false : true}>Sign In</button></Link>
                 </form>
-                <footer className="py-64 px-10 flex justify-center">
+                <footer className="fixed bottom py-10 flex text-2xl">
                     <p>* These fields are mandatory</p>
                 </footer>
             </section>
