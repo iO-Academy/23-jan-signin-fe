9bb64f1ff785c57a2bf3c73dda5095ac82e49fd4
alreuben <ayannahreuben@gmail.com>
2023-04-27 11:42:05 +0100

    styling for admin page

--- src/AdminLoginPage/index.js
+++ src/AdminLoginPage/index.js
@@ -55,16 +55,16 @@ const AdminLoginPage = () => {
     return (
         <>
             <Nav />
-            <section >
-                <div className="px-64">
-                    <p>Enter 4 digit passcode</p>
+            <section className="bg-gray-100 mx-10">
+                <div className="px-64 py-5">
+                    <p className="text-5xl">Enter 4 digit passcode</p>
                 </div>
                 <div className="px-64">
-                    <form className="">
-                        <input onChange={handleInputFirst} value={inputFirst} type="password" maxLength="1" className="border-solid border-2 border-sky-500" />
-                        <input onChange={handleInputSecond} value={inputSecond} type="password" maxLength="1" className="border-solid border-2 border-sky-500" disabled={inputFirst !== '' ? false : true} />
-                        <input onChange={handleInputThird} value={inputThird} type="password" maxLength="1" className="border-solid border-2 border-sky-500" disabled={inputSecond !== '' ? false : true} />
-                        <input onChange={handleInputFourth} value={inputFourth} type="password" maxLength="1" className="border-solid border-2 border-sky-500" disabled={inputThird !== '' ? false : true} />
+                    <form className="py-5">
+                        <input onChange={handleInputFirst} value={inputFirst} type="password" maxLength="1" className="border-solid border-2 border-black rounded-lg h-60 w-44 mx-2 px-10 text-9xl text-center align-middle" />
+                        <input onChange={handleInputSecond} value={inputSecond} type="password" maxLength="1" className="border-solid border-2 border-black rounded-lg h-60 w-44 mx-2 px-10 text-9xl text-center align-middle" disabled={inputFirst !== '' ? false : true} />
+                        <input onChange={handleInputThird} value={inputThird} type="password" maxLength="1" className="border-solid border-2 border-black rounded-lg h-60 w-44 mx-2 px-10 text-9xl text-center align-middle" disabled={inputSecond !== '' ? false : true} />
+                        <input onChange={handleInputFourth} value={inputFourth} type="password" maxLength="1" className="border-solid border-2 border-black rounded-lg h-60 w-44 mx-2 px-10 text-9xl text-center align-middle" disabled={inputThird !== '' ? false : true} />
                     </form>
                 </div>
             </section>
